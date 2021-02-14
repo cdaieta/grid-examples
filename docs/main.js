2975c235ed0ce7f455423459dc7045873b673ce0
David Aieta <cdaieta@gmail.com>
2021-02-14 15:31:58 -0300

    create calendars but not paint

--- docs/main.js
+++ docs/main.js
@@ -1,35 +1,38 @@
-//configuration
-var cfgIniDate = "2021-01-01";
-var cfgEndDate = "2021-03-05";
+var currentState = {
+    currentDate : new Date(),
+    numberOfActiveDays : 30,
+    initialYearMonth: "",
+    endYearMonth: ""
+}
 
 var container;
+var mainContainer = document.querySelector("#main-container");
 
-drawCalendar(cfgIniDate, cfgEndDate);
+// var currentDate = 
 
-function drawCalendar(pIniDate, pEndDate) {
-    var iniDate = new Date(cfgIniDate + "T00:00:00");
-    var endDate = new Date(cfgEndDate + "T00:00:00");
+drawCalendar("2021-01-11", "2021-02-12");
+
+function drawCalendar(pInitialYearMonth, pEndYearMonth) {
+    var firstDay = new Date(getYear(pInitialYearMonth), getMonth(pInitialYearMonth), 1);
+    var lastDay = new Date(getYear(pEndYearMonth), getMonth(pEndYearMonth) + 1, 0);
 
     // Create all dates between startMoht of inidate and end month of enddate
     var isNewMonth = true;
-    var currentDate = iniDate;
+    var currentDate = firstDay;
 
-    // var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
-    // var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-
-    while (currentDate <= endDate) {
+    while (currentDate <= lastDay) {
         //Add Title 
         if (isNewMonth) {
             // Create title of month
             let titleMonth = document.createElement("h1");
-            titleMonth.innerText = currentDate.toLocaleString('default', { month: 'long' });
+            titleMonth.innerText = currentDate.toLocaleString('default', { month: 'long' }) + " " + currentDate.getFullYear();
             titleMonth.classList.add("titleMonth")
-            document.body.appendChild(titleMonth);
+            mainContainer.appendChild(titleMonth);
 
             // create container for new month
             container = document.createElement("section");
             container.classList.add("container")
-            document.body.appendChild(container);
+            mainContainer.appendChild(container);
 
             // leave spaces so beginning starts the correct day of week
             for (let index = 0; index < currentDate.getDay(); index++) {
@@ -47,4 +50,16 @@ function drawCalendar(pIniDate, pEndDate) {
         currentDate.setDate(currentDate.getDate() + 1)
         isNewMonth = (currentDate.getDate() == 1);
     }
+
 }
+
+// "2021-01" => 2021 (number)
+function getYear(pYearMonth) {
+    return parseInt(pYearMonth.substr(0,4));
+}
+
+// "2021-01" => 1 (number)
+function getMonth(pYearMonth) {
+    return parseInt(pYearMonth.substr(5,2));
+}
+
